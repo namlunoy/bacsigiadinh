c34cac083a472787a6f6e14a55684f275b355c2a
conghoang123 <namlunoy@gmail.com>
2015-03-29 18:09:32 +0700

    Cong khoi tao project

--- gen/com/th10/bacsigiadinh/R.java
+++ gen/com/th10/bacsigiadinh/R.java
@@ -10,6 +10,14 @@ package com.th10.bacsigiadinh;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int counter_text_bg=0x7f040004;
+        public static final int counter_text_color=0x7f040005;
+        public static final int list_background=0x7f040001;
+        public static final int list_background_pressed=0x7f040002;
+        public static final int list_divider=0x7f040003;
+        public static final int list_item_title=0x7f040000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -18,30 +26,47 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int counter_bg=0x7f020000;
+        public static final int ic_drawer=0x7f020001;
+        public static final int ic_home=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int ic_map=0x7f020004;
+        public static final int ic_pages=0x7f020005;
+        public static final int ic_tintuc=0x7f020006;
+        public static final int ic_tracuu=0x7f020007;
+        public static final int ic_whats_hot=0x7f020008;
+        public static final int list_item_bg_normal=0x7f020009;
+        public static final int list_item_bg_pressed=0x7f02000a;
+        public static final int list_selector=0x7f02000b;
     }
     public static final class id {
-        public static final int action_settings=0x7f080005;
-        public static final int button1=0x7f080001;
-        public static final int button2=0x7f080004;
-        public static final int checkBox1=0x7f080002;
-        public static final int radioButton1=0x7f080003;
-        public static final int textView1=0x7f080000;
+        public static final int action_settings=0x7f090006;
+        public static final int cc_icon_item_menu=0x7f090004;
+        public static final int cc_linearLayout=0x7f090002;
+        public static final int cc_menu_gridview=0x7f090003;
+        public static final int cc_name_item_menu=0x7f090005;
+        public static final int drawer_layout=0x7f090000;
+        public static final int frame_container=0x7f090001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int fragment_diadiem=0x7f030001;
+        public static final int fragment_tintuc=0x7f030002;
+        public static final int fragment_tracuu=0x7f030003;
+        public static final int item_gridview_drawer=0x7f030004;
+        public static final int item_gridview_drawer_2=0x7f030005;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
+        public static final int action_settings=0x7f060002;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060001;
     }
     public static final class style {
         /** 
@@ -65,10 +90,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
