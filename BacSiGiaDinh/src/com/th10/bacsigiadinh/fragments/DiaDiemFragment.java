10124e393f4c3115acd2b9530760da99bd8874d0
conghoang123 <namlunoy@gmail.com>
2015-04-01 01:24:35 +0700

    cai dat meno chay

--- BacSiGiaDinh/src/com/th10/bacsigiadinh/fragments/DiaDiemFragment.java
+++ BacSiGiaDinh/src/com/th10/bacsigiadinh/fragments/DiaDiemFragment.java
@@ -17,10 +17,14 @@ import android.view.ViewGroup;
 import android.widget.Toast;
 
 public class DiaDiemFragment extends Fragment {
-	public DiaDiemFragment() {
-	}
 
 	View rootView;
+	static final LatLng HAMBURG = new LatLng(53.558, 9.927);
+	static final LatLng KIEL = new LatLng(53.551, 9.993);
+	private GoogleMap map;
+
+	public DiaDiemFragment() {
+	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -29,33 +33,24 @@ public class DiaDiemFragment extends Fragment {
 		rootView = inflater
 				.inflate(R.layout.fragment_diadiem, container, false);
 
+		map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+				.getMap();
+		Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG)
+				.title("Hamburg"));
+		Marker kiel = map.addMarker(new MarkerOptions()
+				.position(KIEL)
+				.title("Kiel")
+				.snippet("Kiel is cool")
+				.icon(BitmapDescriptorFactory
+						.fromResource(R.drawable.ic_launcher)));
+
+		// Move the camera instantly to hamburg with a zoom of 15.
+		map.moveCamera(CameraUpdateFactory.newLatLngZoom(HAMBURG, 15));
+
+		// Zoom in, animating the camera.
+		map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+
 		return rootView;
-	}
-
-	static final LatLng HAMBURG = new LatLng(53.558, 9.927);
-	static final LatLng KIEL = new LatLng(53.551, 9.993);
-	private GoogleMap map;
-
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		Toast.makeText(getActivity(), "onCreate", 1).show();
-		// map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
-		// .getMap();
-		// Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG)
-		// .title("Hamburg"));
-		// Marker kiel = map.addMarker(new MarkerOptions()
-		// .position(KIEL)
-		// .title("Kiel")
-		// .snippet("Kiel is cool")
-		// .icon(BitmapDescriptorFactory
-		// .fromResource(R.drawable.ic_launcher)));
-		//
-		// // Move the camera instantly to hamburg with a zoom of 15.
-		// map.moveCamera(CameraUpdateFactory.newLatLngZoom(HAMBURG, 15));
-		//
-		// // Zoom in, animating the camera.
-		// map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
 	}
 
 }
